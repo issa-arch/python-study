9b586f56437c79a392a4e19932991a6303e58e53
EssaW3A <issa75589@gmail.com>
2024-10-17 16:30:42 +0300

    extra_strings_task.py

---
+++ extra_strings_task.py
@@ -0,0 +1,27 @@
+firstname="essa"
+secondname="ismail"
+fullname=firstname+" "+secondname
+print(fullname)
+
+word= "PYTHON"
+print(word.index("P"))
+print(word.index("N"))
+
+pharse="learning python is fun"
+print(pharse[9:15])
+
+greeting="Hello World"
+print(greeting.replace("World","Python"))
+
+msg="Python Programing"
+print(msg.lower())
+print(msg.upper())
+
+sentence="this is a simple sentence"
+print(len(sentence))
+
+name= "    John Doe    "
+print(name.strip())
+
+quote="The best way to predict the future is to create it."
+print("future" in quote)

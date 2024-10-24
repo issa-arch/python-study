91e45838f7038ae00c82b23b13e4f97c6dddaea4
EssaW3A <issa75589@gmail.com>
2024-10-24 14:01:38 +0300

    my_bs.py

---
+++ tuples.py
@@ -0,0 +1,11 @@
+# create a tuple
+
+marks=(100,300,250,160,450,600)
+print(type(marks))
+print(marks[5])
+# convert to a list
+marks=list(marks)
+print(type(marks))
+# conberting back to a tuple
+marks=tuple(marks)
+print(type(marks))
